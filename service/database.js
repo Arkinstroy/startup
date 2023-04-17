dc2dc003ec67a195bb3c1f66d139f686ce2cd441
Arkinstroy <93345601+Arkinstroy@users.noreply.github.com>
2023-04-17 17:17:44 -0600

    Add service code

---
+++ service/database.js
@@ -0,0 +1,49 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
+const userCollection = client.db('Cluster0').collection('user');
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+    console.log('in create user');
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+
+    console.log(user);
+    return user;
+}
+
+
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+};
